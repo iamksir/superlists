f748720e9112352e1b10b6bfc7c47ff7aa455392
kk <iamksir@163.com>
2022-01-06 22:08:57 +0800

    简单视图函数单元测试通过

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 #django2.X把django.core.urlresolvers包更改为了django.urls包
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 # Create your tests here.
 
 class HOmePageTest(TestCase):
@@ -9,3 +10,10 @@ class HOmePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>worked</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
