f668227319147a09eb2dde53c8d36c35232a0a1b
kk <iamksir@163.com>
2022-01-06 21:51:45 +0800

    第一个视图函数单元测试通过

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
-
+#django2.X把django.core.urlresolvers包更改为了django.urls包
+from django.urls import resolve
+from lists.views import home_page
 # Create your tests here.
 
-class SmokeTest(TestCase):
+class HOmePageTest(TestCase):
 
-    def test_bad_mpths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
