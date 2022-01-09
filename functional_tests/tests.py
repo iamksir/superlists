226643e54f09ae18ff947d4c9f34a7be5a95b3b6
kk <iamksir@163.com>
2022-01-09 22:17:07 +0800

    使用模板继承

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -36,14 +36,14 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')#assertRegex价差字符串和正则表达式是否匹配
-        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        self.check_for_row_in_list_table('1:Buy peacock feathers')
-        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         self.browser.quit()
         self.browser = webdriver.Chrome()
